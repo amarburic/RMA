72492b456962f1688ae047d7c21840daf8fe34fb
Amar.B <amaron_96@live.com>
2017-03-13 23:42:49 +0100

    Vjezba 1; Zadatak 1 i 2

---
+++ rma17-17401/app/src/main/java/ba/unsa/etf/rma/amar_buric/rma17_17401/Pocetni.java
@@ -0,0 +1,43 @@
+package ba.unsa.etf.rma.amar_buric.rma17_17401;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class Pocetni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pocetni);
+
+        Button dugme = (Button)findViewById(R.id.button);
+        final TextView tekstPolje = (TextView)findViewById(R.id.editText);
+        ListView lista = (ListView)findViewById(R.id.listView);
+
+        final ArrayList<String> unosi = new ArrayList<String>();
+
+        final ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, unosi);
+
+        lista.setAdapter(adapter);
+
+        dugme.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                unosi.add(0,tekstPolje.getText().toString());
+                adapter.notifyDataSetChanged();
+                tekstPolje.setText("");
+            }
+        });
+
+
+
+    }
+}
